e642e7aebecbf6c6d72794611bce58e78e67a25f
SKBk-tg <kslesarev69@gmail.com>
2023-08-15 10:04:53 +0300

    On branch main 	modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <div>Home</div>;
+	try {
+		return <div>Home</div>;
+	} catch(e) {
+		return <div>Error 404</div>;
+		console.log(e);
+	}
+
 };
 
 export default Home;
